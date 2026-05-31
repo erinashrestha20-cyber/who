50635f59aa0c67d781fc22b23657471e601519ff
erinashrestha20-cyber <erinashrestha20@gmailcom>
2026-05-31 10:21:35 +0545

    new file created

--- ReactJS/js/Count.js
+++ ReactJS/js/Count.js
@@ -2,4 +2,6 @@ let number=0;
 function increaseCount(){
     number = number + 1;
     document.getElementById("count").innerText = number;
+    document.getElementById("count").style.color= "navy";
+    document.getElementById("count").style.fontSize = "80px";
 }
---
+++ ReactJS/js/scripts.js
@@ -0,0 +1,88 @@
+//step1: make element of all the id present in html
+const tweetInput = document.getElementById("tweet-input");
+const charCounter = document.getElementById("char-counter");
+const postBtn = document.getElementById("post-btn");
+const draftMsg = document.getElementById("draft-msg");
+const clearBtn = document.getElementById("clear-btn");
+
+const MAX_CHARS = 150;
+
+tweetInput.addEventListener("keyup", function(){
+    const currentLength = tweetInput.value.length;
+
+    charCounter.textContent = currentLength + "/" + MAX_CHARS;
+
+    //change color based on limit
+    if (currentLength > MAX_CHARS) {
+        // if length increase from 150
+        charCounter.style.color = "#e74c3c";
+        postBtn.disabled = true;
+    } else if (currentLength === 0){
+        // if no text was written
+        charCounter.style.color = "#2ecc71";
+        postBtn.disabled = true;
+    } else {
+            // if text value is less than 150 and is not 0
+        charCounter.style.color = "#2ecc71";
+        postBtn.disabled = false;
+    }
+});
+
+tweetInput.addEventListener("change",function(){
+
+    if(tweetInput.value.length > 0){
+        draftMsg.textContent = "Draft Saved!";
+
+        setTimeout(function(){
+            draftMsg.textContent = "";
+        }, 2000);
+    }
+
+});
+
+postBtn.addEventListener("click",function(){
+    const tweetText = tweetInput.value;
+
+    alert("You posted:" + tweetText);
+
+    tweetInput.value = "";
+    charCounter.textContent = "0 /" + MAX_CHARS;
+    charCounter.style.color = "#2ecc71";
+    postBtn.disabled = true;
+    draftMsg.textContent = "";
+});
+
+clearBtn.addEventListener("click", function(){
+
+    // clear the input field
+    tweetInput.value = "";
+
+    // reset counter
+    charCounter.textContent = "0/" + MAX_CHARS;
+
+    // reset color
+    charCounter.style.color = "#2ecc71";
+
+    // disable post button again
+    postBtn.disabled = true;
+
+    // clear draft message
+    draftMsg.textContent = "";
+  
+});
+
+tweetInput.addEventListener("keyup", function(){
+
+const currentLength = tweetInput.value.length;
+
+charCounter.textContent = currentLength + "/" + MAX_CHARS;
+
+    // enable/disable clear button
+    if(currentLength === 0){
+        clearBtn.disabled = true;
+    }
+    else{
+        clearBtn.disabled = false;
+    }
+
+});
